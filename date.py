6823b95f23aa8dda7902ffb0fa2b560cf4374443
Michael Süß <shanachan@sysmike.de>
2013-05-01 19:34:08 +0200

    small code examplex added

---
+++ date.py
@@ -0,0 +1,8 @@
+﻿from datetime import datetime
+
+now = datetime.now()
+print(now)
+print(now.day)
+print(now.month)
+print str(now.month) + "/" + str(now.day) + "/" + str(now.year)
+print str(now.month) + "/" + str(now.day) + "/" + str(now.year) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
