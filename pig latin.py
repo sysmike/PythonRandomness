bb36c93a06f5844721c9d96f5cb6700edccf7c93
Michael Süß <shanachan@sysmike.de>
2013-05-04 00:57:34 +0200

    added pig latin.py

---
+++ pig latin.py
@@ -0,0 +1,18 @@
+﻿pyg = 'ay'
+
+original = raw_input('Enter a word to be translated into Pig Latin: ')
+
+if len(original) > 0 and original.isalpha():
+	word = original
+	word.lower()
+	first = word[0]
+	print "Original: " + original
+	if first == "a" and "e" and "i" and "o" and "u":
+		new_word = word + pyg
+		print "Result: " + new_word
+	else:
+		new_word = original
+		new_word = new_word[1:] + first + pyg
+		print "Result: " + new_word
+else:
+	print 'empty'
